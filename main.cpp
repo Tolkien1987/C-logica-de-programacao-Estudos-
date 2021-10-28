e2e891ff7ecffaf1adde475489e64330f8ca50be
Fabio_Martinez <rok5150@gmail.com>
2021-10-28 14:28:55 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int age;
+  float average;
+  
+  cout << "Digite a sua idade: ";
+  cin >> age;
+  cout << "Digite sua média:" ;
+  cin >> average;
+
+  cout << "Sua idade é = " << age << endl;
+  cout << " e a média é de = " << average << " anos" << endl;
+  
+	return 0;
+} 
