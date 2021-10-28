55ce378f8686e0f23ebbc91bf4a2da476c767b84
Fabio_Martinez <rok5150@gmail.com>
2021-10-28 19:48:57 +0000

    Exercice#4

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  
-  int myAge = 20;
-  int mySisterAgo = 33;
+//Faça um programa que leia 5 valores int, e mostre quais são pares
 
-  if (myAge <= mySisterAgo) { 
-    cout << "I am younger" << endl;
+int main() {
+
+  int a, b, c, d, e;
+  int qtd = 0;
+
+  cin >> a >> b >> c >> d >> e;
+
+  if (a % 2 == 0) {
+    qtd++;
   }
-  
+  if (b % 2 == 0) {
+    qtd++;
+  }
+  if (c % 2 == 0) {
+    qtd++;
+  }
+  if (d % 2 == 0) {
+    qtd++;
+  }
+  if (e % 2 == 0) {
+    qtd++;
+  }
+
+  cout << qtd << " valor(s) pares" << endl;
+
 	return 0;
 } 
