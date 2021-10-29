dadaa7cf3d88de204bcae1a809309432e5c0d3a4
Fabio_Martinez <rok5150@gmail.com>
2021-10-29 23:31:58 +0000

    Exercises

--- main.cpp
+++ main.cpp
@@ -28,3 +28,154 @@ int main() {
 
 	return 0;
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// - - - - - - //
+
+// int main() {
+
+//   bool verdadeiro = 12 < 13 || 10 <= 9;
+//   cout << "verdadeiro = " << verdadeiro << endl;
+//   int myAge = 20;
+//   int mySisterAgo = 33;
+
+//   if (myAge <= mySisterAgo) { 
+//     cout << "I am younger" << endl;
+//   }
+
+// 	return 0;
+//}
+
+
+// - - - - - - - - //
+
+// #include <iostream>
+// using namespace std;
+
+// //Faça um programa que leia 5 valores int, e mostre quais são pares
+
+// int main() {
+
+//   int myAge = 20;
+//   int mySisterAgo = 33;
+
+//   if (myAge <= mySisterAgo) { 
+//     cout << "I am younger" << endl;
+//   int a, b, c, d, e;
+//   int qtd = 0;
+
+//   cin >> a >> b >> c >> d >> e;
+
+//   if (a % 2 == 0) {
+//     qtd++;
+//   }
+//   if (b % 2 == 0) {
+//     qtd++;
+//   }
+//   if (c % 2 == 0) {
+//     qtd++;
+//   }
+
+//   if (d % 2 == 0) {
+//     qtd++;
+//   }
+//   if (e % 2 == 0) {
+//     qtd++;
+//   }
+
+//   cout << qtd << " valor(s) pares" << endl;
+
+// 	return 0;
+// } 
+
+
+// - - - - - - //
+
+
+// #include <iostream>
+// using namespace std;
+
+// //Faça um programa que leia 5 valores int, e mostre quais são pares
+// //Faça um programa que mostre pares entre 1...100
+
+// int main() {
+
+//   int a, b, c, d, e;
+//   int qtd = 0;
+
+//   cin >> a >> b >> c >> d >> e;
+
+//   if (a % 2 == 0) {
+//     qtd++;
+//   }
+//   if (b % 2 == 0) {
+//     qtd++;
+//   }
+//   if (c % 2 == 0) {
+//     qtd++;
+//   }
+//   if (d % 2 == 0) {
+//     qtd++;
+//   }
+//   if (e % 2 == 0) {
+//     qtd++;
+
+//   for (int i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//       cout << i << endl;
+//     }
+//   }
+
+//   cout << qtd << " valor(s) pares" << endl;
+
+// 	return 0;
+// }  
+
+
+// - - - - - //
+
+
+// int main() {
+
+//   for (int i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//   for (int i = 2; i <= 100; i+=2) {   
+//       cout << i << endl;
+//     }
+//   }
+
+// 	return 0;
+// }  
+
+
+
+// - - - -  //
+
+// int main() {
+
+//   for (int i = 2; i <= 100; i+=2) {   
+//       cout << i << endl;
+//     }
+
+//   int i = 2;
+
+//   while ( i <= 100) {
+//     cout << i << endl;
+//     i += 2;
+//   }
+
+// 	return 0;
+// }  
