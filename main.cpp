8bc69872d2fbfe4f6eaba0dba363f23ff9b60f79
Fabio_Martinez <rok5150@gmail.com>
2021-11-03 13:37:42 +0000

    day2 - exercise7

--- main.cpp
+++ main.cpp
@@ -35,6 +35,27 @@ int main() {
 
 
 
+// _ _ _ _ _ _ _
+
+
+
+// #include <iostream>
+// using namespace std;
+ 
+// int main() {
+//     cout << "\n\n Print the sum of two numbers :\n";
+// 	  cout << "-----------------------------------\n";
+// 	  int a;
+// 	  int b;
+// 	  int sum;
+// 	  a=29;
+// 	  b=30;
+// 	  sum=a+b;
+// 	  cout << " The sum of "<< a << " and "<<b <<" is : "<< sum <<"\n\n" ;
+// }
+
+
+
 // - - - - - - //
 
 // int main() {
