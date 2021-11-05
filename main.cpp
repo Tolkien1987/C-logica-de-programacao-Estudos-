b7752f129e49e3694913ecc74902b34b54a9c168
Fabio_Martinez <rok5150@gmail.com>
2021-11-05 19:05:47 +0000

    Day3 - Organization

--- main.cpp
+++ main.cpp
@@ -2,36 +2,53 @@
 #include <iomanip>
 using namespace std;
 
-//Lista pessoal de exercícios com C++ 
-
-void printValue(float newIncome, float readjustment, int percent) {
-  cout << fixed << setprecision(2);
-  cout << "New income " << newIncome << endl;
-  cout << "Readjustment " << readjustment << endl;
-  cout << "Percent " << percent << " %" << endl;
-}
 
 int main() {
-    float value;
-    cin >> value;
+  
 
-    if (value >= 0 && value <= 400.00) {
-        printValue(value * 1.15, value * 0.15, 15);
 
-    } else if (value >= 400.01 && value <= 800.00) {
-        printValue(value * 1.12, value * 0.12, 12);
-    } else if (value >= 800.01 && value <= 1200.00) {
-        printValue(value * 1.10, value * 0.10, 10);
-    } else if (value >= 1200.01 && value <= 2200.00) {
-        printValue(value * 1.07, value * 0.07, 7);
-    } else if (value > 2000.00) {
-        printValue(value * 1.04, value * 0.04, 4);
-    }
 
 	return 0;
 } 
 
 
+
+// _ _ _ _ _ _ _ //
+
+
+
+// #include <iostream>
+// #include <iomanip>
+// using namespace std;
+
+// //Lista pessoal de exercícios com C++ 
+
+// void printValue(float newIncome, float readjustment, int percent) {
+//   cout << fixed << setprecision(2);
+//   cout << "New income " << newIncome << endl;
+//   cout << "Readjustment " << readjustment << endl;
+//   cout << "Percent " << percent << " %" << endl;
+// }
+
+// int main() {
+//     float value;
+//     cin >> value;
+
+//     if (value >= 0 && value <= 400.00) {
+//         printValue(value * 1.15, value * 0.15, 15);
+
+//     } else if (value >= 400.01 && value <= 800.00) {
+//         printValue(value * 1.12, value * 0.12, 12);
+//     } else if (value >= 800.01 && value <= 1200.00) {
+//         printValue(value * 1.10, value * 0.10, 10);
+//     } else if (value >= 1200.01 && value <= 2200.00) {
+//         printValue(value * 1.07, value * 0.07, 7);
+//     } else if (value > 2000.00) {
+//         printValue(value * 1.04, value * 0.04, 4);
+//     }
+
+// 	return 0;
+// } 
 
 
 
@@ -58,6 +75,7 @@ int main() {
 
 // - - - - - - //
 
+
 // int main() {
 
 //   bool verdadeiro = 12 < 13 || 10 <= 9;
@@ -74,8 +92,6 @@ int main() {
 
 
 // - - - - - - - - //
-
-
 
 
 
